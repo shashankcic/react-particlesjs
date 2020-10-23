40bbad8422963733144c508b5126f10763d8fd34
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-10-23 18:57:43 +0530

    complete web app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import particlesConfig from './config/particlesConfig';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ position: 'relative', overflow: "hidden" }}>
+      <div style={{ position: 'absolute'}}>
+        <Particles height="100vh" width="100vw" params={particlesConfig} />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
